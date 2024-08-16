994e1c4f9c995b3e16baf5d288ffde1571f3e808
Duc.NT <311020ntd@gmail>
2024-08-16 21:09:55 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int number = 1;
+int i =2;
+do
+{
+    Console.WriteLine("Enter a number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number >= 2)
+    {
+        if (number % i == 0)
+        {
+            Console.WriteLine(number + " is not a prime");
+            break;
+        }
+        else
+        {
+            Console.WriteLine(number + " is a prime");
+        }
+        i++;
+    }else
+    {
+        Console.WriteLine(number + " is not a prime");
+    }
+
+} while (i <= Math.Sqrt(number));
+
